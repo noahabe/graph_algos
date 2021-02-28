b726f841bc7f5c6762b5820bd0c27f3c2dd5560f
Noah Abe <noahabe64@gmail.com>
2021-02-28 21:57:47 +0300

    made it more of weighted.

--- weighted.h
+++ weighted.h
@@ -1,12 +1,24 @@
 #include <iostream>
 #include <vector>
 
+// WGraph for Weighted Graph
+
 class WGraph;
 std::istream& operator>>(std::istream& is, WGraph& G);
 
+// recall that the edge is a directed edge.
+struct Edge {
+	int dest; // the destination of the edge
+	int weight;
+	Edge(int d, int w)
+	:dest(d),weight(w) 
+	{}
+};
+
+
 class WGraph { 
 public:
-	std::vector<std::vector<int>> v;
+	std::vector<std::vector<Edge>> v;
 	std::vector<int>& operator[](int x) {
 		return v[x];
 	}
@@ -16,12 +28,13 @@ public:
 std::istream& operator>>(std::istream& is, WGraph& G) {
 	int n,m;
 	int u1,u2; /* vertices */
+	int w; // the weight of the edge (u1,u2)
 	
 	is >> n >> m; /* number of vertices, number of edges */
 	G.v.resize(n+1);
 	for (int _ = 1; _ <= m; ++_) {
-		is >> u1 >> u2;
-		G[u1].push_back(u2);
+		is >> u1 >> u2 >> w;
+		G[u1].emplace_back(u2,w);
 	}	
 	return is;
 }
@@ -32,8 +45,8 @@ std::ostream& operator<<(std::ostream& os,WGraph& H) {
 	for_each_vertex(u,H) {
 		os << "vertex: " << u << " | ";
 		
-		for (int v : H[u]) {
-			os << v << " ";	
+		for (const Edge& v : H[u]) {
+			os << v.dest << " ";	
 		}
 		os << std::endl;
 	}
@@ -44,8 +57,8 @@ WGraph Transpose(WGraph& G){
 	WGraph retval;
 	retval.v.resize(G.order()+1);
 	for_each_vertex(v,G) {
-		for (int u : G[v]) {
-			retval[u].push_back(v);
+		for (const Edge& e : G[v]) {
+			retval[e.dest].emplace_back(v,e.weight);
 		}	
 	}
 	return retval;
