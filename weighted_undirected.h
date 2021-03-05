1a6aa2f2ae32cbc8345b22518910ca31704627ae
Noah Abe <noahabe64@gmail.com>
2021-03-05 10:56:45 +0300

    added a function called get_all_edges

--- weighted_undirected.h
+++ weighted_undirected.h
@@ -1,30 +1,56 @@
+/* WUGraph - Weighted Undirected Graph */
+
 #include <iostream>
 #include <vector>
 
 class WUGraph;
 std::istream& operator>>(std::istream& is, WUGraph& G);
 
+struct Edge {
+	int vertex1;
+	int vertex2;
+	int weight;
+	
+	Edge(int s,int d,int w)
+	:vertex1(s),vertex2(d),weight(w)
+	{}	
+
+	bool operator<(Edge& rhs) { 
+		return this->weight < rhs.weight;
+	}
+};
+
 class WUGraph { 
 private:
-	std::vector<std::vector<int>> v;
+	std::vector<std::vector<Edge>> v;
 public:
 	friend std::istream& operator>>(std::istream& is, WUGraph& G);
-	std::vector<int>& operator[](int x) {
+	std::vector<Edge>& operator[](int x) {
 		return v[x];
 	}
 	int order() { return v.size() - 1; } 
+	std::vector<Edge> get_all_edges(); 
 };
+
+std::vector<Edge> WUGraph::get_all_edges() {
+	std::vector<Edge> retval;
+	retval.reserve(this->order()/2); // this is just my herueistics
+	for_each_vertex(u,(*this))
+		for (Edge& e:v[u])
+			retval.push_back(e);
+	return retval;	
+}
 
 std::istream& operator>>(std::istream& is, WUGraph& G) {
 	int n,m;
-	int u1,u2; /* vertices */
+	int u1,u2,w; /* vertices & weight */
 	
 	is >> n >> m; /* number of vertices, number of edges */
 	G.v.resize(n+1);
 	for (int _ = 1; _ <= m; ++_) {
-		is >> u1 >> u2;
-		G.v[u1].push_back(u2);
-		G.v[u2].push_back(u1);	
+		is >> u1 >> u2 >> w;
+		G.v[u1].emplace_back(u1,u2,w);
+		G.v[u2].emplace_back(u1,u2,w);	
 	}	
 	return is;
 }
